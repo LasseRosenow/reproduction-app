88e80c97b95d25cee4719fa93560dc0978099403
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2023-08-02 12:55:20 +0200

    Test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from "next"
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    icons: [
+      {
+        url: "logo.svg",
+        type: "image/xml+svg",
+        sizes: "any",
+      },
+    ],
+  }
+}
+
 export default function RootLayout({ children }) {
   return (
     <html>
